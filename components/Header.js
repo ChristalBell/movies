5fd76f00bca4803dd1782ed822885284c1830fb6
Christal Bell <christalvictoriabell@gmail.com>
2023-11-27 21:41:29 -0800

    profile doesnt work

--- components/Header.js
+++ components/Header.js
@@ -5,15 +5,15 @@ const Header = () => {
   return (
     <div>
       <div>
-        <Image src="/movie.png" alt="home" width={4} height={4} />
-        <Image src="/squares.png" alt="squares" width={4} height={4} />
-        <Image src="/film.png" alt="film" width={4} height={4} />
-        <Image src="/television.png" alt="tv" width={4} height={4} />
-        <Image src="/bookmark.png" alt="saved" width={4} height={4} />
-        <Image src="/profile.png" alt="profile" width={4} height={4} />
+        <Image src="/assets/movie.png" alt="home" width={20} height={20} />
+        <Image src="/assets/squares.png" alt="squares" width={20} height={20} />
+        <Image src="/assets/film.png" alt="film" width={20} height={20} />
+        <Image src="/assets/television.png" alt="tv" width={20} height={20} />
+        <Image src="/assets/bookmark.png" alt="saved" width={20} height={20} />
+        <Image src="/assets/profile.png" alt="profile" width={20} height={20} />
       </div>
       <div>
-        <Image src="/search.png" alt="search" width={4} height={4} />
+        <Image src="/assets/search.png" alt="search" width={20} height={20} />
         <input type="text" placeholder="Search for movies or TV series" />
       </div>
     </div>
