342fb48d575a910d8eef83b2470b2294df51313f
Christal Bell <christalvictoriabell@gmail.com>
2024-01-10 08:31:31 -0800

    map works

--- components/Body.js
+++ components/Body.js
@@ -5,24 +5,26 @@ import React, { useState, useEffect } from "react";
 const Body = () => {
   const [shows, setShows] = useState([
     {
-      name: "",
-      season: 0,
-      number: 0,
-      rating: { average: 0 },
-      image: { medium: "" },
-      summary: "",
+      show: {
+        name: "",
+        season: 0,
+        number: 0,
+        rating: { average: 0 },
+        image: { medium: "" },
+        summary: "",
+      },
     },
   ]);
 
   const showList = [];
 
   useEffect(() => {
-    fetch("https://api.tvmaze.com/singlesearch/shows?q=:query")
+    fetch("https://api.tvmaze.com/search/shows?q=the")
       .then((response) => response.json())
       .then((data) => setShows(data));
   }, []);
 
-  console.log({ shows });
+  console.log(shows);
 
   return (
     <div style={{ backgroundColor: "#171d2f", color: "white" }}>
@@ -32,16 +34,9 @@ const Body = () => {
       </div>
       <div>
         <h2>Trending</h2>
-{/* 
-        let totalShowList = showList.map(function(element) {
-          return `${element.name}`;
-        })
-        console.log(totalShowList);      */}
-
-let allShows = shows.map(function(element){
-  return `${Element.name}`;
-})
-console.log(allShows);
+        {shows.map((show) => {
+          return <h3 key={show.show.name}> {show.show.name} </h3>;
+        })}
       </div>
     </div>
   );
