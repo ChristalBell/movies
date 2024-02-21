1cc9746228b3c89d5467a55accb010284e8192e1
Christal Bell <christalvictoriabell@gmail.com>
2024-02-21 09:29:31 -0800

    header done

--- components/Header.js
+++ components/Header.js
@@ -13,9 +13,15 @@ const Header = () => {
     >
       <div
         className="header-first"
-        style={{ paddingRight: "2rem", paddingLeft: "2rem" }}
+        style={{ paddingRight: ".15rem", paddingLeft: ".15rem" }}
       >
-        <Image src="/assets/movie.png" alt="home" width={20} height={20} />
+        <Image
+          src="/assets/movie.png"
+          alt="home"
+          width={20}
+          height={20}
+          style={{ marginRight: "4rem" }}
+        />
       </div>
 
       <Image
@@ -23,19 +29,31 @@ const Header = () => {
         alt="squares"
         width={20}
         height={20}
-        style={{ marginRight: "2rem" }}
+        style={{ marginRight: "1rem" }}
       />
       <Image
         src="/assets/film.png"
         alt="film"
         width={20}
         height={20}
-        style={{ marginRight: "2rem" }}
+        style={{ marginRight: "1rem" }}
       />
-      <Image src="/assets/television.png" alt="tv" width={20} height={20} />
-      <Image src="/assets/bookmark.png" alt="saved" width={20} height={20} />
+      <Image
+        src="/assets/television.png"
+        alt="tv"
+        width={20}
+        height={20}
+        style={{ marginRight: "1rem" }}
+      />
+      <Image
+        src="/assets/bookmark.png"
+        alt="saved"
+        width={20}
+        height={20}
+        style={{ marginRight: "1rem" }}
+      />
 
-      <div style={{ paddingLeft: "2rem", paddingRight: "2rem" }}>
+      <div style={{ paddingLeft: "3rem", paddingRight: ".15rem" }}>
         <Image src="/assets/user.png" alt="profile" width={20} height={20} />
       </div>
     </div>
